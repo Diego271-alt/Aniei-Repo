1a674d553309b86200ce019751ce026b85b7285b
Diego Romero <dr87152@gmail.com>
2020-10-29 22:16:10 -0600

    Updates

---
+++ precache-manifest.c74bcfa9bc0d347a715cea58969a6a5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb61c5fc9eae65f5f90ceaea05acf883",
+    "url": "/Aniei-Repo/index.html"
+  },
+  {
+    "revision": "05f0ef68d4650b8e5601",
+    "url": "/Aniei-Repo/static/css/main.5a787452.chunk.css"
+  },
+  {
+    "revision": "abdb01541d966fbd740f",
+    "url": "/Aniei-Repo/static/js/2.49851ff6.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/Aniei-Repo/static/js/2.49851ff6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05f0ef68d4650b8e5601",
+    "url": "/Aniei-Repo/static/js/main.14b9e316.chunk.js"
+  },
+  {
+    "revision": "8f6e7fdd04b38eaa6056",
+    "url": "/Aniei-Repo/static/js/runtime-main.5d1d50a5.js"
+  },
+  {
+    "revision": "8b7c9843e542e295f891b6e0507e8f42",
+    "url": "/Aniei-Repo/static/media/pp.8b7c9843.jpg"
+  }
+]);
